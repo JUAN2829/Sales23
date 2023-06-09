f08c9c5f5cf423ecdb76e99c8ad11b8c7f81aedd
JUAN2829 <76124000+JUAN2829@users.noreply.github.com>
2023-06-08 22:10:45 -0600

    Add user photo

--- Sales/Sales.API/Controllers/CitiesController.cs
+++ Sales/Sales.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
 using Sales.API.Helpers;
@@ -8,6 +10,7 @@ using Sales.Shared.Entities;
 namespace Sales.API.Controllers
 {
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("/api/cities")]
     public class CitiesController : ControllerBase
     {
@@ -66,6 +69,18 @@ namespace Sales.API.Controllers
 
             return Ok(city);
         }
+
+     // combo para cargar ciudades
+
+        [AllowAnonymous]
+        [HttpGet("combo/{stateId:int}")]
+        public async Task<ActionResult> GetCombo(int stateId)
+        {
+            return Ok(await _context.Cities
+                .Where(x => x.StateId == stateId)
+                .ToListAsync());
+        }
+
 
         [HttpPost]
         public async Task<ActionResult> PostAsync(City city)
